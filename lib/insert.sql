591d239f065af0e66e1aca6216a795ad47792982
Milena Kari <milenakari@yahoo.com>
2019-01-22 19:38:17 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,13 @@
 INSERT INTO series (title, author_id,subgenre_id) VALUES
 ('Harry Potter', 1, 1),
 ('Magic Kitten', 2, 2);
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES 
+(1, "Lady", "Woof Woof", "direwolf", 1, 1), 
+(2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), 
+(3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), 
+(4, "Eddard Stark", "Winter is coming", "human", 1, 1),
+(5, "Eddard Stark", "Winter is coming", "human", 1, 1),
+(6, "Eddard Stark", "Winter is coming", "human", 1, 1),
+(7, "Eddard Stark", "Winter is coming", "human", 1, 1),
+(8, "Eddard Stark", "Winter is coming", "human", 1, 1);
