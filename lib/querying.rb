0ad10009a58361c2d4eb9c14d1e1378d58d89ff2
Milena Kari <milenakari@yahoo.com>
2019-01-22 20:33:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,11 +13,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name 
-   FROM authors 
+  "SELECT authors.name, subgenres.name
+   FROM authors
      INNER JOIN series
         ON authors.id = series.author_id
-     INNER JOIN subgenres 
+     INNER JOIN subgenres
         ON subgenres.id = series.subgenre_id  ;"
 
 end
