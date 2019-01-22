22af957e888ec4d2225708cf95e04cd4e4ce7582
Milena Kari <milenakari@yahoo.com>
2019-01-22 20:40:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id ORDER BY COUNT(species = 'human') DESC LIMIT 1;"
+  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id ORDER BY COUNT(characters.species = 'human') DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
