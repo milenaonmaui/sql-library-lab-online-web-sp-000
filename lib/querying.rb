e20e7945c056b434c074d814aaead51c680ccc61
Milena Kari <milenakari@yahoo.com>
2019-01-22 20:20:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT name, COUNT(species) as num from characters GROUP BY species ORDER BY num DESC LIMIT 1;"
+  "SELECT name, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
+  
 end
 
 def select_name_and_series_subgenres_of_authors
